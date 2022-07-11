a40189e90d22a6f1d936288a0fb9088613a34801
MaxPolevoy <Lidiya-1958>
2022-07-11 13:25:20 +0300

    homework10 Задача 2

--- src/by/tms/myproject/lessons10/homework/Task02.java
+++ src/by/tms/myproject/lessons10/homework/Task02.java
@@ -14,10 +14,54 @@ package by.tms.myproject.lessons10.homework;
 
 
 public class Task02 {
-
     public static void main(String[] args) {
-//        int[] numer = {1000, 500, 100, 50, 10, 5, 1};
-//        String[] liter = {M, D, C, L, X, V, I};I
+        System.out.println(romanInDecimal("IV"));
     }
 
+    private static int letterInNumber(char letter) {
+        switch (letter) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return -1;
+        }
+    }
+
+    public static int romanInDecimal(String roman) {
+        int integerValue = 0;
+
+        for (int i = 0; i < roman.length(); i++) {
+            char ch = roman.charAt(i);
+            int number = letterInNumber(ch);
+            if (number == -1) {
+                throw new NumberFormatException("Недопустимый формат");
+            }
+
+            if (i + 1 < roman.length()) {
+                int nextNumber = letterInNumber(roman.charAt(i + 1));
+                if (nextNumber > number) {
+                    integerValue += (nextNumber - number);
+                    i++;
+                } else {
+                    integerValue += number;
+                }
+            } else {
+                integerValue += number;
+
+            }
+        }
+        return integerValue;
+    }
 }
